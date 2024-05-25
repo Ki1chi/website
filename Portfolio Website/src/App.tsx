d8b8ec35455db37940e56987914adcf64e000fb5
Nori Terada <noriterada19@gmail.com>
2024-05-24 23:10:17 -0600

    Added bg image, nav bar, and main text

--- Portfolio Website/src/App.tsx
+++ Portfolio Website/src/App.tsx
@@ -1,12 +1,25 @@
 // import { useState } from 'react'
-import './App.css'
+import './App.scss'
 
 function App() {
 
 
+
   return (
     <>
-    
+    <body className="bg-image"></body>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+
+    </nav>
+    <main>
+      <h1 className="main-text">Hello,</h1>
+      </main>
     </>
   )
 }

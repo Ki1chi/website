264cf41a7bbf5254b5f13ed4841e364a16d0c174
Nori Terada <noriterada19@gmail.com>
2024-05-28 06:18:35 -0600

    -fixed grid jank -added project page -add media query for about page

--- Portfolio Website/src/App.tsx
+++ Portfolio Website/src/App.tsx
@@ -23,17 +23,21 @@ function App() {
       <h1 className='main-text'>Front-end Web Developer.</h1>
       <h3 className="main-text2">Nori Terada</h3>
       </main>
-      <section className='about-section'>
+      <section className='about-section-container'>
+        <div className='about-section'>
           <h1 className='item-3'>About me</h1>
           <h3 className='item-4'> Hello! I'm Nori Terada, a passionate self-taught developer with a knack for creating visually appealing and user-friendly websites. My journey into web development began with computers. A few years ago I got curious on how websites work, which quickly evolved into my passion and hopefully a career.  </h3>
           <h1 className='item-1'>My Journey</h1>
-          <h3 className='item-2'> Without a formal background in computer science, I took the plunge into coding by using online resources, tutorials, and the passion to learn. I started with HTML and CSS, applying as much as I could learn to build many websites. As I got the hang of those two I ventured into Javascript to make a truly interactive experience. I felt that javascript wasn't enough for alot of my designs so I learned React and many other technologies to help me truly build elegant and responsive websites.</h3>
+          <h3 className='item-2'> Without a formal background in computer science, I took the plunge into coding by using online resources, tutorials, and the passion to learn. I started with HTML and CSS, applying as much as I could learn to build many websites. As I got the hang of those two I ventured into Javascript to make a truly interactive experience.</h3>
           <h1 className='item-5'>My Philosopy</h1>
-          <h3 className='item-6'>I believe in continuously learning. I think staying updated with the latest technologies and trends is what a web developer should truly strive for. my self-taught background has given me many opportunities to seek out such technologies. I believe being self-taught has instilled in me a strong problem-solving mindset and the ability to adapt to many new challenges quickly. I will always aim for high quality in my projects I attempt.</h3> 
-          {/* <h3>What I can do-here are some of the following technologies I have utilized.</h3>
-          <h3>projects</h3>
-          <h3>contact</h3> */}
+          <h3 className='item-6'>I believe in continuously learning. I like staying updated with the latest technologies and trends. My self-taught background has given me many opportunities to seek out such technologies. I believe being self-taught has instilled in me a strong problem-solving mindset and the ability to adapt to many new challenges quickly.</h3> 
+          </div>
       </section>
+      <section className='project-section'>
+          <h3>here are some of the following technologies I have utilized.</h3>
+          <h3>projects</h3>
+      </section>
+      {/* <h3>contact</h3> */}
     </>
   )
 }

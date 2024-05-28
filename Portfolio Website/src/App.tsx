a20e32a0756af459d27029de81d518067710f2fb
Nori Terada <noriterada19@gmail.com>
2024-05-28 03:06:36 -0600

    Fixed styles and added new ones

--- Portfolio Website/src/App.tsx
+++ Portfolio Website/src/App.tsx
@@ -7,18 +7,22 @@ function App() {
 
   return (
     <>
-    <body className="bg-image"></body>
-    <nav>
-      <ul>
-        <li>Home</li>
+    <img className="bg-image"></img>
+    <nav className='nav-container'>
+      <div className='logo-container'>
+        <div className='logo'></div>
+      </div>
+      <ul className='nav-bar'>
+        {/* <li>Home</li> */}
         <li>About</li>
         <li>Projects</li>
         <li>Contact</li>
       </ul>
-
     </nav>
-    <main>
-      <h1 className="main-text">Hello,</h1>
+    <main className="main-container">
+      {/* <h3 className="main-text">Hey, I am</h3> */}
+      <h1 className='name'>Front-end Web Developer</h1>
+      <h3 className="main-text">My name is Nori, I like to make websites</h3>
       </main>
     </>
   )

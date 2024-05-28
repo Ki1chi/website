6ce8fe858b0a9524892b551ddb716d0d1da18848
Nori Terada <noriterada19@gmail.com>
2024-05-28 06:04:54 -0600

    added grid design for main section

--- Portfolio Website/src/App.tsx
+++ Portfolio Website/src/App.tsx
@@ -24,14 +24,15 @@ function App() {
       <h3 className="main-text2">Nori Terada</h3>
       </main>
       <section className='about-section'>
-        <div>
-          <h3>About me- Hello! I'm Nori Terada, a passionate self-taught developer with a knack for creating visually appealing and user-friendly websites. My journey into web development began with computers. A few years ago I got curious on how websites work, which quickly evolved into my passion and hopefully a career.  </h3>
-          <h3>My Journey- Without a formal background in computer science, I took the plunge into coding by using online resources, tutorials, and the passion to learn. I started with HTML and CSS, applying as much as I could learn to build many websites. As I got the hang of those two I ventured into Javascript to make a truly interactive experience. I felt that javascript wasn't enough for alot of my ideas so I learned React and many other technologies to help me truly build elegant and responsive websites.</h3>
-          <h3>What I can do-here are some of the following technologies I have utilized.</h3>
-          <h3>My philosopy-I believe in continuously learning. I think staying updated with the latest technologies and trends is what a web developer should truly strive for. my self-taught background has given me many opportunities to seek out such technologies. I believe being self-taught has instilled in me a strong problem-solving mindset and the ability to adapt to many new challenges quickly. I will always aim for high quality in my projects I attempt.</h3>
+          <h1 className='item-3'>About me</h1>
+          <h3 className='item-4'> Hello! I'm Nori Terada, a passionate self-taught developer with a knack for creating visually appealing and user-friendly websites. My journey into web development began with computers. A few years ago I got curious on how websites work, which quickly evolved into my passion and hopefully a career.  </h3>
+          <h1 className='item-1'>My Journey</h1>
+          <h3 className='item-2'> Without a formal background in computer science, I took the plunge into coding by using online resources, tutorials, and the passion to learn. I started with HTML and CSS, applying as much as I could learn to build many websites. As I got the hang of those two I ventured into Javascript to make a truly interactive experience. I felt that javascript wasn't enough for alot of my designs so I learned React and many other technologies to help me truly build elegant and responsive websites.</h3>
+          <h1 className='item-5'>My Philosopy</h1>
+          <h3 className='item-6'>I believe in continuously learning. I think staying updated with the latest technologies and trends is what a web developer should truly strive for. my self-taught background has given me many opportunities to seek out such technologies. I believe being self-taught has instilled in me a strong problem-solving mindset and the ability to adapt to many new challenges quickly. I will always aim for high quality in my projects I attempt.</h3> 
+          {/* <h3>What I can do-here are some of the following technologies I have utilized.</h3>
           <h3>projects</h3>
-          <h3>contact</h3>
-        </div>
+          <h3>contact</h3> */}
       </section>
     </>
   )
